116ed4af41add7f3809148bf72ee776e820b2638
Dmytro Mykytenko <50519414+dmykyten@users.noreply.github.com>
2021-12-15 12:34:56 +0200

    Update goal_tracker.py

--- goal_tracker.py
+++ goal_tracker.py
@@ -1,46 +1,24 @@
 import pandas as pd
-import argparse
-import sys
-
-def parse_input():
-    parser = argparse.ArgumentParser(description="Simple goal tracker")
-
-    subparsers = parser.add_subparsers(help='Sub-commands', required=True, dest='command')
-    add_parser = subparsers.add_parser('add', help='Add new goal')
-    complete_parser = subparsers.add_parser('get_complete', help='Get complete goals')
-    active_parser = subparsers.add_parser('get_active', help='Get active goals')
-    failed_parser = subparsers.add_parser('get_failed', help='Get failed goals')
-    exit_parser = subparsers.add_parser('exit', help='Exit')
-
-    add_parser.add_argument('desc', help='Description')
-    add_parser.add_argument('amount', help='Amount of goal')
 
 
-    args = parser.parse_args()
-    command = args.subparser
-    if command == 'add':
-        # function for creating goal???
-        # func(args['desc'], args['amount'])
-        pass
+def work_with_csv(file):
+    df = pd.read_csv(file)
+    print(df)
+    column = input()
+    row = input()
+    df[column] += row
+    if df[column] > 1:
+        df["Success"] += "+"
+    elif df[column] < 1:
+        df["Failure"] += "+"
+    print(df)
+    return df
 
-    elif command == 'get_complete':
-        # get_complete() -> dataframe with completed
-        pass
-    elif command == 'get_active':
-        # get_complete() -> dataframe with active
-        pass
-    elif command == 'get_failed':
-        # get_complete() -> dataframe with failed
-        pass
-    elif command == 'exit':
-        exit()
-
-def exit():
-    sys.exit()
 
 def main():
-    parse_input()
+    file_name = input()
+    work_with_csv(file_name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
