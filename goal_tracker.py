876f0353af951e8c29fb83be95b475c80e3ff47f
Dmytro Mykytenko <50519414+dmykyten@users.noreply.github.com>
2021-12-15 12:14:22 +0200

    Added commands for argparse

--- goal_tracker.py
+++ goal_tracker.py
@@ -1,8 +1,41 @@
 import pandas as pd
+import argparse
+import sys
 
+def parse_input():
+    parser = argparse.ArgumentParser(description="Simple goal tracker")
+
+    subparsers = parser.add_subparsers(help='Sub-commands', required=True, dest='command')
+    add_parser = subparsers.add_parser('add', help='Add new goal')
+    complete_parser = subparsers.add_parser('get_complete', help='Get complete goals')
+    active_parser = subparsers.add_parser('get_active', help='Get active goals')
+    failed_parser = subparsers.add_parser('get_failed', help='Get failed goals')
+    exit_parser = subparsers.add_parser('exit', help='Exit')
+
+    add_parser.add_argument('desc', help='Description')
+    add_parser.add_argument('amount', help='Amount of goal')
+
+
+    args = parser.parse_args()
+    command = args.subparser
+    if command == 'add':
+        # function for creating goal???
+        # func(args['desc'], args['amount'])
+        pass
+
+    elif command == 'get_complete':
+        # get_complete() -> 
+    elif command == 'get_failed':
+        pass
+    elif command == 'exit':
+        exit()
+
+def exit():
+    sys.exit()
 
 def main():
-    pass
+    parse_input()
+
 
 if __name__ == '__main__':
     main()
